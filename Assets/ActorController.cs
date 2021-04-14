837a86b1cf3b169c06edcc0274cc381925c597a6
ordinary-student-geng <gengmengtao@126.com>
2021-04-14 21:53:56 +0800

    add falling and roll

--- Assets/ActorController.cs
+++ Assets/ActorController.cs
@@ -7,37 +7,97 @@ public class ActorController : MonoBehaviour
     public GameObject model;
     public float walkSpeed;
     public float runSpeed;
+    public float jumpVelocity = 3.0f;
+    public float rollVelocity = 4.0f;
+    public PlayerInput pi;
 
-    private PlayerInput _pi;
+
     private Animator _anim;
     private Rigidbody _rigbody;
-    private Vector3 _movingVector;
+    private Vector3 _planarVector;
+    private Vector3 _thrustVector; // push in vertical direction
+    public bool _lockplanar = false;
     // Start is called before the first frame update
-    void Awake()
+    private void Awake()
     {
-        _pi = GetComponent<PlayerInput>();
+        pi = GetComponent<PlayerInput>();
         _anim = model.GetComponent<Animator>();
         _rigbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         //animation
-        _anim.SetFloat("forward", Mathf.Lerp(_anim.GetFloat("forward"), _pi.dMag * (_pi.run ? 2.0f : 1.0f), 0.4f));
-        if(_pi.jump) _anim.SetTrigger("jump");
+        _anim.SetFloat("forward", Mathf.Lerp(_anim.GetFloat("forward"), pi.dMag * (pi.run ? 2.0f : 1.0f), 0.4f));
+        if(pi.jump) _anim.SetTrigger("jump");
 
-        if(_pi.dMag > 0.01f)
+        if(pi.dMag > 0.01f)
         {
-            Vector3 targetForward = Vector3.Slerp(model.transform.forward, _pi.dVec, 0.3f);
+            Vector3 targetForward = Vector3.Slerp(model.transform.forward, pi.dVec, 0.3f);
             model.transform.forward = targetForward;
         }
+
+        if(_rigbody.velocity.magnitude > 0f)
+        {
+            _anim.SetTrigger("roll");
+        }
+
+        if (!_lockplanar)
+        {
+            _planarVector = pi.dMag * model.transform.forward * walkSpeed * (pi.run ? runSpeed : 1.0f);
+        }
             
-        _movingVector = _pi.dMag * model.transform.forward * walkSpeed * (_pi.run ? runSpeed : 1.0f);
     }
 
     private void FixedUpdate()
     {
-        _rigbody.position += _movingVector * Time.fixedDeltaTime;
+        //_rigbody.position += _planarVector * Time.fixedDeltaTime;
+        _rigbody.velocity = new Vector3(_planarVector.x, _rigbody.velocity.y, _planarVector.z) + _thrustVector;
+        _thrustVector = Vector3.zero;
     }
+
+    #region handle messages
+
+    public void OnJumpEnter()
+    {
+        _thrustVector = new Vector3(0, jumpVelocity, 0);
+        _lockplanar = true;
+        pi.inputEnabled = false;
+    }
+
+    public void OnJumpExit()
+    {
+        
+    }
+
+    public void isOnGround()
+    {
+        _anim.SetBool("isOnGround", true);
+    }
+
+    public void NotOnGround()
+    {
+        _anim.SetBool("isOnGround", false);
+    }
+
+    public void OnGroundEnter()
+    {
+        _lockplanar = false;
+        pi.inputEnabled = true;
+    }
+
+    public void OnFallingEnter()
+    {
+        _lockplanar = true;
+        pi.inputEnabled = false;
+    }
+
+    public void OnRollEnter()
+    {
+        _thrustVector = new Vector3(0, rollVelocity, 0);
+        _lockplanar = true;
+        pi.inputEnabled = false;
+    }
+    #endregion
 }
---
+++ Assets/OnGroundSensor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundSensor : MonoBehaviour
+{
+
+    public CapsuleCollider capsule;
+    public float offset = -0.1f;
+
+    private Vector3 _point1;
+    private Vector3 _point2;
+    private float _radius;
+
+    private void Awake()
+    {
+        _radius = capsule.radius - 0.05f;
+    }
+
+    private void FixedUpdate()
+    {
+        _point1 = transform.position + transform.up * (_radius + offset);
+        _point2 = transform.position + transform.up * (capsule.height - _radius + offset);
+
+        Collider[] colliders = Physics.OverlapCapsule(_point1, _point2, _radius, LayerMask.GetMask("Ground"));
+        if(colliders.Length > 0)
+        {
+            SendMessageUpwards("isOnGround");
+        }
+        else
+        {
+            SendMessageUpwards("NotOnGround");
+        }
+
+
+    }
+}
